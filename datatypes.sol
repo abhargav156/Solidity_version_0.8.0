f42cf294a85d506b16145f2854a5c33d8e663f1a
abhargav156 <abhargav156@gmail.com>
2022-10-09 16:37:36 +0530

    Solidity 1

---
+++ datatypes.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+ 
+ 
+contract DataTypes{
+    bool public trueorfalse = false;
+
+    uint public OnlyNumbers = 7;
+  
+    bytes4 public AnyText;
+
+    int public OnlyNumbers2 = 5;
+
+    address public hashtypecode = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+
+}
